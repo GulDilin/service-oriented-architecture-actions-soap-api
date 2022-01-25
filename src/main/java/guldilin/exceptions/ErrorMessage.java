b91be0c424280f0385b4457a799e645c687edfe8
GulDilin <zhenyagurin@gmail.com>
2022-01-25 03:25:18 +0300

    Init services

---
+++ src/main/java/guldilin/exceptions/ErrorMessage.java
@@ -0,0 +1,9 @@
+package guldilin.exceptions;
+
+public class ErrorMessage {
+    public static final String IS_INTEGER = "Need to be integer";
+    public static final String NOT_FOUND = "Entry not found";
+    public static final String CITY_NOT_FOUND = "City not found";
+    public static final String COORDINATES_NOT_FOUND = "Coordinates not found";
+    public static final String STORAGE_SERVICE_REQUEST_FAILED = "Storage service request failed";
+}
